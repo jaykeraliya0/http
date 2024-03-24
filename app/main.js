88a569c832dceea3d26551f326a0232dd99bb014
Jay Keraliya <jaykeraliya0@gmail.com>
2024-03-24 10:09:40 +0530

    Responded with 200

--- app/main.js
+++ app/main.js
@@ -1,10 +1,13 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
   socket.on("close", () => {
     socket.end();
     server.close();
   });
 });
 
+console.log("Server listening on port 4221");
 server.listen(4221, "localhost");
