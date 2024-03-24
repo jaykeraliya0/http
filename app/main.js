11d40d3f35918f24daa9a0ad143aec61ec683adc
Jay Keraliya <jaykeraliya0@gmail.com>
2024-03-24 11:03:21 +0530

    GET File

--- app/main.js
+++ app/main.js
@@ -1,21 +1,35 @@
 const net = require("net");
+const fs = require("fs");
+const path = require("path");
 
-const server = net.createServer((socket) => {
+const server = net.createServer(async (socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
     console.log(request);
-    const path = data.toString().split("\r\n")[0].split(" ")[1];
+    const reqPath = data.toString().split("\r\n")[0].split(" ")[1];
 
-    if (path === "/") {
+    if (reqPath === "/") {
       socket.write("HTTP/1.1 200 OK \r\n\r\n");
-    } else if (path.startsWith("/echo/")) {
-      const message = path.split("/echo/")[1];
+    } else if (reqPath.startsWith("/echo/")) {
+      const message = reqPath.split("/echo/")[1];
       const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
       socket.write(response);
-    } else if (path === "/user-agent") {
+    } else if (reqPath === "/user-agent") {
       const userAgent = request.split("User-Agent: ")[1].split("\r\n")[0];
       const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
       socket.write(response);
+    } else if (reqPath.startsWith("/files/")) {
+      const dir = process.argv[2] === "--directory" ? process.argv[3] : null;
+      const fileName = reqPath.split("/files/")[1];
+      const filePath = path.join(dir, fileName);
+
+      if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath);
+        const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`;
+        socket.write(response);
+      } else {
+        socket.write("HTTP/1.1 404 Not Found \r\n\r\n");
+      }
     } else {
       socket.write("HTTP/1.1 404 Not Found \r\n\r\n");
     }
